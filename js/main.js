3c618e83c68861ed863286f909150c2942af43c3
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-01-17 00:42:50 +0530

    Dark theme added

--- js/main.js
+++ js/main.js
@@ -440,3 +440,27 @@ $(function() {
 
 });
 
+function setTheme(themeName) {
+	localStorage.setItem('theme', themeName);
+	document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+	if (localStorage.getItem('theme') === 'theme-dark') {
+			setTheme('theme-light');
+	} else {
+			setTheme('theme-dark');
+	}
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+	if (localStorage.getItem('theme') === 'theme-dark') {
+			setTheme('theme-dark');
+			$("#slider").attr('checked',true);
+	} else {
+			setTheme('theme-light');
+	}
+})();
+
